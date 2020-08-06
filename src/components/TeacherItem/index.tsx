2a02c8644433d69f6fe504b69f692a3efbebf1b6
Diogo Manoel Pereira dos Santos <diogo.santtosicet@gmail.com>
2020-08-06 12:35:00 -0400

    Criacao da Rota de Connectios

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 
 import './styles.css';
+
 
 export interface Teacher {
         id: number,
@@ -19,6 +21,13 @@ interface TeacherItemProps {
 }
 
 const TeacherItem: React.FunctionComponent<TeacherItemProps> = ({teacher}) =>{
+
+    function createNewConnection() {
+        api.post('/connections', {
+            user_id: teacher.id
+        })
+    }
+
     return (
         <article className="teacher-item">
                     <header>
@@ -38,7 +47,7 @@ const TeacherItem: React.FunctionComponent<TeacherItemProps> = ({teacher}) =>{
                             Preço/Hora
                             <strong>R$ {teacher.cost}</strong>
                         </p>
-                        <a href={`https://wa.me/${teacher.whatsapp}>`} type="button">
+                        <a onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`} target="blank" type="button">
                             <img src={whatsappIcon} alt="Whatsapp"/>
                             Entrar em Contato
                         </a>
